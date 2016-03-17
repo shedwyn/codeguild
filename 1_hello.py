5d4241685c96eb68730ceb82896f2e61551909d9
elfough <shedwyn@gmail.com>
2016-03-17 10:27:16 -0700

    hello print

---
+++ 1_hello.py
@@ -0,0 +1,15 @@
+print("\nReady to begin?  If not, oh well...\n")
+
+#miles/miles_per_gallon
+#total_gal*cost_gas
+
+#miles = input("How many miles will you travel? ")
+#miles_p_gal = input("How many miles per gallon do you get? ")
+#total_gal = input ("How much does your tank hold? ")
+#cost_gas = input("What is the estimated price per gallon? ")
+
+print('What is your name?')
+name = input()
+greeting = 'So nice to meet you, ' + name
+
+print(greeting)
