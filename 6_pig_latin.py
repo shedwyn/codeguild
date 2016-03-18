941d254484a3d3d234addfd81abd16e479c6b920
elfough <shedwyn@gmail.com>
2016-03-18 11:17:02 -0700

    pig latin exercise 1st challenge

---
+++ 6_pig_latin.py
@@ -0,0 +1,26 @@
+#Pig Latin translator
+#enter sentence
+#translate to pig latin
+#print out
+
+#bonus/advanced - handle vowels with punctuation, punctuation, handle capitalization
+
+#take first letter
+#move to end
+#add 'ay' on new end
+
+#'cat' - 'atcay'
+
+english_word = input('Insert a word (all lower, no punc): ')
+
+#slice first letter
+
+english_word_list = list(english_word)
+
+english_word_list.append(english_word_list.pop(0))
+english_word_list.append('ay')
+
+pig_latin_word = ''.join(english_word_list)
+
+
+print(pig_latin_word)
