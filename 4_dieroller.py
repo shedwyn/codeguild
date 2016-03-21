233386d894bbbf68e6f651f96a49a9329f53db87
elfough <shedwyn@gmail.com>
2016-03-21 15:21:44 -0700

    4 die roller with functions added and working

--- 4_dieroller.py
+++ 4_dieroller.py
@@ -4,14 +4,36 @@
 #Print sum all dies
 import random
 
-num_rolls = 0
-num_dice = int(input('how many dice '))
-sum_dice = 0
+def user_input_die_number():
+    """"ask user how many dice s/he wants to roll. Return as integer."""
+    return int(input('How many dice would you like to roll? '))
 
-while num_rolls < num_dice:
-    dice_roll = random.randint(1,6)
-    print('roll value:', dice_roll)
-    sum_dice += dice_roll
-    num_rolls += 1
+def roll_6_side_dice():
+    """roll a 6 sided die.  return value."""
+    return random.randint(1, 6)
 
-print('the die sum =', sum_dice, 'the average is', sum_dice/num_dice)
+def sum_tally_roll_values(list_of_values):
+    """adds up all the values on a list.  Returns sum."""
+    return sum(list_of_values)
+
+def record_roll_values(roll_values):
+    dice_roll = roll_6_side_dice()
+    roll_values.append(dice_roll)
+    return roll_values
+
+number_of_rolls = user_input_die_number()
+roll_count = 0
+roll_values = []
+
+while roll_count != number_of_rolls:
+    roll_values = record_roll_values(roll_values)
+    roll_count += 1
+
+tally_of_dice = sum_tally_roll_values(roll_values)
+
+average_of_rolls = tally_of_dice/number_of_rolls
+
+print('\nYou rolled ' + str(roll_count) + ' times',\
+    '\n\nHere is the list of your rolls:', roll_values, \
+    '\n\nThe total value of the rolls was:', tally_of_dice, \
+    '\n\nThe average value of your rolls was:', average_of_rolls)
