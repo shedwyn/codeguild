5beb9aeb0a0fea02e71e207fec19f1a512af1f9c
elfough <shedwyn@gmail.com>
2016-03-21 12:03:36 -0700

    non-working version, includes some functions.

--- 3_random.py
+++ 3_random.py
@@ -6,25 +6,35 @@ import random
 #comp output of guess high or low
 #once guess is correct, quit
 
-print('let\'s play a game, i will pick a number between 1 ' +
-    'and 100, and then you will try to guess it')
 
-ran_num = random.randint(1, 100)
-guess_num = 0
+def gen_secret_num():
+    """generate a random number between 1 and 50. Return it"""
+    return random.randint(1, 50)
 
-#guess_num = int(input("guess a number between 1 and 100: "))
+def prompt_user_to_guess():
+    """prompt user to guess a number between 1 and 50.  Return it as an integer."""
+    return int(input("Guess a number between 1 and 50: "))
 
-while guess_num != ran_num:
-    guess_num = int(input("guess a number between 1 and 100: "))
+def determine_if_guess_correct_or_no(secret_num, ran_num):
+    """determines if the user guess is too high or low or correct.  Print message for user."""
     if guess_num > ran_num:
         print('too high, go lower')
     elif guess_num < ran_num:
         print('too low, go higher')
-    else:
-        print('you got it! Now go away!')
+
+def guess_game_run_loop():
+    print('let\'s play a game, i will pick a number between 1 ' +
+    'and 50, and then you will try to guess it')
+    ran_num = gen_secret_num()
+    guess_num = prompt_user_to_guess()
+    while guess_num != ran_num:
+        determine_if_guess_correct_or_no(guess_num, ran_num)
+    print ('You got it!')
+
+guess_game_run_loop()
 
 
-
+#guess_num = int(input("guess a number between 1 and 100: "))
 
 #is there a way to make this more concise and not \
 #repeat myself as often? may require rewriting the \
