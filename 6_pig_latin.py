04251d0d30afe46d448f202e51c0e9424234dc3a
elfough <shedwyn@gmail.com>
2016-03-22 11:25:21 -0700

    6 pig latin translate with functions and notes, including sugg for improvement.

--- 6_pig_latin.py
+++ 6_pig_latin.py
@@ -1,39 +1,42 @@
 #Pig Latin translator
 
+##consider adding an if statement for line break if len(pig_latin_word_list) > int(x)
 
-def prompt_user_sentence():
-    """prompt the user to enter a sentence.  return the sentence."""
+def prompt_user_for_sentence():
+    """prompt the user to enter a sentence as a string.  return the sentence."""
     return input('Please type a sentence you wish to translate: \n')
 
-def convert_sentence_into_word_list(sentence_string):
-    new_list = sentence_string.split()
+def convert_english_sentence_into_word_list(english_sentence):
     """break the user sentence into individual words to be translated. return the list of words"""
+    new_list = english_sentence.split()
     return new_list
 
-def remove_endword_punctuation(word):
-    """tests the final char of a word for punctuation.  returns that value."""
+def return_endword_punctuation(word):
+    """tests the final char of a word for punctuation.  returns the punctuation or 
+    an empty string if no punctuation as a value."""
     punctuation_list = ['!', '?', '.', ';', ':', '\'', '"', '_', '-']
     if word[-1] in punctuation_list:
         punctuation_char = word[-1]
     else:
         punctuation_char = ""
     return punctuation_char
-
-def translate_word_to_pig_latin(word):
-    """takes a word, tests it against the rules of pig latin, and translates.  returns the pig latin word"""
+#see note in translate_word_to_pig_latin regarding 'my'
+def translate_word_to_pig_latin(english_word):
+    """takes a english_word, tests it against the rules of pig latin, and translates.  
+    returns the pig latin.   problem with word 'my'"""
     vowels_list = ['a', 'e', 'i', 'o', 'u']
 
-    first_letter = word[0]
+    first_letter = english_word[0]
     default_pig_ending = 'ay'
     vowel_word_pig_ending = 'yay'
 
-    if len(word) == 1:
+    if len(english_word) == 1:
         second_letter = ""
         remainder_letters = ""
     else:
-        second_letter = word[1]
-        remainder_letters = word[2:]
-
+        second_letter = english_word[1]
+        remainder_letters = english_word[2:]
+    #Need to figure out how to deal with 2 character strings where 'y' is second letter, as with 'my'
     if first_letter not in vowels_list and second_letter not in vowels_list:
         pig_latin_word = remainder_letters + first_letter + second_letter + default_pig_ending
 
@@ -45,33 +48,36 @@ def translate_word_to_pig_latin(word):
 
     return pig_latin_word
  
-def create_pig_latin_word_list(word_list):
-    
-    list_of_pig_latin_words = []
+def create_pig_latin_word_list(english_sentence_word_list):
+    """takes in the list of english words, translates each word using pig latin rules, and
+    adds it to the pig latin list of words.  returns the pig latin list of words."""
+    pig_latin_word_list = []
 
-    for word in word_list:
+    for word in english_sentence_word_list:
 
-        word_final_punctuation = remove_endword_punctuation(word)
+        word_final_punctuation = return_endword_punctuation(word)
 
         if word_final_punctuation != "":
             word = word[0:-1]
 
         pig_latin_word = translate_word_to_pig_latin(word) + word_final_punctuation
 
-        list_of_pig_latin_words.append(pig_latin_word)
+        pig_latin_word_list.append(pig_latin_word)
 
-    return list_of_pig_latin_words
+    return pig_latin_word_list
 
-def combine_words_into_sentence(word_list):
+def combine_words_into_sentence(pig_latin_sentence_word_list):
     """combines each word from a list into a sentence string.  returns the sentence."""
-    pig_sentence = ' '.join(word_list)
+    pig_sentence = ' '.join(pig_latin_sentence_word_list)
     return pig_sentence
 
 
-english_sentence = prompt_user_sentence()
+english_sentence = prompt_user_for_sentence()
 
-list_of_english_sentence_words = convert_sentence_into_word_list(english_sentence)
+english_sentence_word_list = convert_english_sentence_into_word_list(english_sentence)
 
-pig_latin_sentence = combine_words_into_sentence(create_pig_latin_word_list(list_of_english_sentence_words))
+pig_latin_word_list = create_pig_latin_word_list(english_sentence_word_list)
+
+pig_latin_sentence = combine_words_into_sentence(pig_latin_word_list)
 
 print('translates into: ', pig_latin_sentence)
