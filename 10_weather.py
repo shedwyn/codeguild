7a085826b340b7894759f60c85de208c73ea4607
elfough <shedwyn@gmail.com>
2016-03-30 11:28:52 -0700

    10 weather, final version with all nec. tasks complete

--- 10_weather.py
+++ 10_weather.py
@@ -1,6 +1,8 @@
 #CHECK! parse and store the **daily total** data for each day.  
-#find and print out hte specific date with the most rain.
-#find and print out the year with the most rain.
+#CHECK! find and print out hte specific date with the most rain.
+#CHECK! find and print out the year with the most rain.
+#Advanced options and other challenges listed at bottom of script
+
 
 import urllib.request
 
@@ -9,14 +11,14 @@ def intro_message():
     print (
         'This program uses the Sunnyside record as of the current date.\n' +
         'Therefore, if you run it on a different day you will ' +
-        'return different information'
+        'return different information\n\n'
     )
     return None
 
 def download_sunnyside_record():
     """opens weather record, imports as utf-8 list of string 
     lines.  returns that list."""
-    with urllib.request.urlopen('http://or.water.usgs.gov/non-usgs/bes/sunnyside.rain') as weather_import_file:
+    with urllib.request.urlopen('https://raw.githubusercontent.com/selassid/codeguild/master/sunnyside.rain') as weather_import_file:
         rain_date_lines = [byte_line.decode('utf-8')for byte_line in weather_import_file]
     return rain_date_lines      
 
@@ -37,7 +39,7 @@ def transform_line_to_pair(line):
     month_total_single_pair_with_ints = convert_listpair_item_to_int(month_total_single_pair)
     return month_total_single_pair_with_ints
 
-def comprehend_date_raintotal_pairs(rain_date_lines):
+def create_list_days_with_daytotals(rain_date_lines):
     """takes in list of lines, comprehends pairs from lines,  returns list of pairs."""
     date_raintotal_pairs = [transform_line_to_pair(line) for line in rain_date_lines[12:]]
     return date_raintotal_pairs
@@ -51,16 +53,6 @@ def create_dates_to_daytotals(dates_with_daytotals):
         dates_to_daytotals[pair[0]] = pair[1]
     return dates_to_daytotals
 
-def find_date_with_max_rain(dates_to_daytotals):
-    """takes in the dict, sorts on key.  returns keys sorted list"""
-    sorted_word_to_counts = sorted(
-        dates_to_daytotals, 
-        key=dates_to_daytotals.get, 
-        reverse=True
-    )
-    date_with_max_rain = sorted_word_to_counts[0] + ' had the most rain with ' + str(dates_to_daytotals[sorted_word_to_counts[0]] / 100) + ' inches.'
-    return date_with_max_rain
-
 def convert_date_into_yearonly(pair):
     """takes in list of pairs.  takes pair[0], splits on '-', saves only year.
     returns refurbished pair."""
@@ -69,23 +61,27 @@ def convert_date_into_yearonly(pair):
     
     return pair
 
-def sum_year_totals():  #START HERE!
+def create_years_to_yeartotals(dates_with_daytotals):
     """total all daytotals for each year"""
     #need to breakout pair[0] in list into single key with all values for keys matching
-    dictionary = {}
-    for pair in otherdictionary
-        if otherdictionary[pair[0]] not in dictionary[]:
-            dictionary[pair[0]] = otherdictionary[pair[0]]
+    years_with_daytotals = [convert_date_into_yearonly(pair) for pair in dates_with_daytotals]
+    years_to_yeartotals = {}
+    for pair in years_with_daytotals:
+        if pair[0] not in years_to_yeartotals:
+            years_to_yeartotals[pair[0]] = pair[1]
         else:
-            dictionary[pair[0]] = dictionary[pair[0]] + otherdictionary[pair[0]]
+            years_to_yeartotals[pair[0]] += pair[1]
+    return years_to_yeartotals
 
-print(convert_date_into_yearonly(['01-OCT-2011', 293]))
-
-# def split_dates_into_years(date_raintotal_pairs):
-#     """takes in list of pairs, convert first item of pair to year only.  return list of pairs."""
-#     for pair in date_raintotal_pairs
-
-#     return year_raintotal_pairs
+def find_date_with_max_rain(dictionary_with_raintotals):
+    """takes in the dict, sorts on key.  returns keys sorted list"""
+    sorted_word_to_counts = sorted(
+        dictionary_with_raintotals, 
+        key=dictionary_with_raintotals.get, 
+        reverse=True
+    )
+    date_with_max_rain = sorted_word_to_counts[0] + ' had the most rain with ' + str(dictionary_with_raintotals[sorted_word_to_counts[0]] / 100) + ' inches.'
+    return date_with_max_rain
 
 def overall_schematic():
     """contains all the functions that run this program.  
@@ -93,30 +89,28 @@ def overall_schematic():
     print('\n\n')
     intro_message()
     rain_date_lines = download_sunnyside_record()
-    dates_with_daytotals = comprehend_date_raintotal_pairs(rain_date_lines)
+    dates_with_daytotals = create_list_days_with_daytotals(rain_date_lines)
     dates_to_daytotals = create_dates_to_daytotals(dates_with_daytotals)
-    date_with_max_rain =find_date_with_max_rain(dates_to_daytotals)
+    years_to_yeartotals = create_years_to_yeartotals(dates_with_daytotals)
 
-    print('\n\n', date_with_max_rain, '\n\n')
+    date_with_max_rain = find_date_with_max_rain(dates_to_daytotals)
+    year_with_max_rain = find_date_with_max_rain(years_to_yeartotals)
 
+    print(date_with_max_rain, '\n\n')
 
-   
-    # create another list with lists of dates that end with same
-    #     year and related daily total(s)
+    print(year_with_max_rain, '\n\n')
 
-    #     find count of the daily totals within each list in that
-    #         list and print that total with the associated year 
-    #         from the outer dict
-    #         list = [keyyear:[10, 5, 7, 0, 23], keyyear2:[5, 8, 9, 22, 0]]
-
-
-    # find max()
-
-    return dates_to_daytotals
-
+    return None
 
 overall_schematic()
 
-
-
+#Find and print the day of the year with the most rain on average. 
+    #E.g. December 30th has 1" of rain on average.
+#Allow someone to type in a date in the future and, using the average 
+    #value for that day of the year, predict the amount of rain.
+#My thoughts:
+#How can I change create_dates_to_daytotals(dates_with_daytotals)
+    #so it can process year as well?
+#How could this program change to make it more functional for a user?
+    #ie allowing user to indicate date or year to pull data
 
