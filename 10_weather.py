2ac604f0bd4df41baa7374b5f2d81b25f0a58bb9
elfough <shedwyn@gmail.com>
2016-03-29 22:38:17 -0700

    10 weather finds date with most rain, beginning code for annual rain

--- 10_weather.py
+++ 10_weather.py
@@ -9,7 +9,7 @@ def intro_message():
     print (
         'This program uses the Sunnyside record as of the current date.\n' +
         'Therefore, if you run it on a different day you will ' +
-        'return different information\n\n'
+        'return different information'
     )
     return None
 
@@ -51,25 +51,56 @@ def create_dates_to_daytotals(dates_with_daytotals):
         dates_to_daytotals[pair[0]] = pair[1]
     return dates_to_daytotals
 
-def overall_schematic(date):
+def find_date_with_max_rain(dates_to_daytotals):
+    """takes in the dict, sorts on key.  returns keys sorted list"""
+    sorted_word_to_counts = sorted(
+        dates_to_daytotals, 
+        key=dates_to_daytotals.get, 
+        reverse=True
+    )
+    date_with_max_rain = sorted_word_to_counts[0] + ' had the most rain with ' + str(dates_to_daytotals[sorted_word_to_counts[0]] / 100) + ' inches.'
+    return date_with_max_rain
+
+def convert_date_into_yearonly(pair):
+    """takes in list of pairs.  takes pair[0], splits on '-', saves only year.
+    returns refurbished pair."""
+    splitdate = pair[0].split('-')[2]
+    pair = [splitdate, pair[1]]
+    
+    return pair
+
+def sum_year_totals():  #START HERE!
+    """total all daytotals for each year"""
+    #need to breakout pair[0] in list into single key with all values for keys matching
+    dictionary = {}
+    for pair in otherdictionary
+        if otherdictionary[pair[0]] not in dictionary[]:
+            dictionary[pair[0]] = otherdictionary[pair[0]]
+        else:
+            dictionary[pair[0]] = dictionary[pair[0]] + otherdictionary[pair[0]]
+
+print(convert_date_into_yearonly(['01-OCT-2011', 293]))
+
+# def split_dates_into_years(date_raintotal_pairs):
+#     """takes in list of pairs, convert first item of pair to year only.  return list of pairs."""
+#     for pair in date_raintotal_pairs
+
+#     return year_raintotal_pairs
+
+def overall_schematic():
     """contains all the functions that run this program.  
     returns None."""
+    print('\n\n')
     intro_message()
     rain_date_lines = download_sunnyside_record()
     dates_with_daytotals = comprehend_date_raintotal_pairs(rain_date_lines)
     dates_to_daytotals = create_dates_to_daytotals(dates_with_daytotals)
-    max_val = max(dates_to_daytotals.key()) + ' had the most rain with ' max(dates_to_daytotals.values()) + ' inches')
-    print(max_val)
-    print(dates_to_daytotals[date])
+    date_with_max_rain =find_date_with_max_rain(dates_to_daytotals)
 
-    
-    # remove date and daily total
-    #     sunnyside_record_today[0] + sunnyside_record_today[1]
-    #     date, daily_total 
-    
-    # find max(daily_total) in list
-    #     return with associated date
+    print('\n\n', date_with_max_rain, '\n\n')
 
+
+   
     # create another list with lists of dates that end with same
     #     year and related daily total(s)
 
@@ -84,7 +115,7 @@ def overall_schematic(date):
     return dates_to_daytotals
 
 
-overall_schematic('01-OCT-2011')
+overall_schematic()
 
 
 
