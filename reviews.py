49d7dfa3972d50785cfea0c547a1950a1d83ff93
elfough <shedwyn@gmail.com>
2016-04-05 10:02:09 -0700

    reviews, latest version 16/4/5, 10:02

--- reviews.py
+++ reviews.py
@@ -20,6 +20,16 @@ class Business:
 
     def __str__(self):
         """magic string to format print feature"""
+        return 'Reviews for {}: {})'.format(self.business_name, self.reviews_fields)
+
+
+    def find_avg_rating(self, reviews_fields):
+        """takes reviews fields for business, calc avg on rating, return avg"""
+
+        rating_sum = sum(review_item[0] for review_item in reviews_fields)
+        rating_avg = rating_sum/len(reviews_fields)
+
+        return rating_avg
 
         #  CHRIS:  I am thinking we might be able to do this better - unpack and format the list of reviews
             #  into individual lines.  What do you think?
@@ -29,20 +39,25 @@ class Business:
     
 # ********************** END OF CLASSES *****************************
 
-# def prompt_for_business_name():
-#     """ask user for business name, return business name"""
-#     return business_name = input('Business Name:  ')
+def prompt_for_business_name():
+    """ask user for business name, return business name"""
+    business_name = input('Business Name:  ')
+    return business_name
 
 
 def format_single_review(review_item):
     
     single_review = Review(review_item['rating'], review_item['text'])
     
+    print('Single Review: ', single_review)
+    
     return single_review
 
 def formatting_reviews(list_reviews):
      
-    business_reviews = [format_single_review(review_item) for review_item in list_reviews]          
+    business_reviews = [format_single_review(review_item) for review_item in list_reviews]
+
+    print('Business Reviews:', business_reviews)          
 
     return business_reviews
 
@@ -50,13 +65,20 @@ def format_single_business(business_item):
 
     business_reviews = formatting_reviews(business_item['reviews'])
     single_business = Business(business_item['business_name'], business_reviews)
+    print('Single Business:  ', single_business)
 
     return single_business
 
 def formatting_businesses(raw_business_review_data):
     
-    business_to_reviews = {format_single_business(business_item) for business_item in raw_business_review_data}
-            
+    business_to_reviews = {
+        format_single_business(
+            business_item
+            )
+        for business_item in raw_business_review_data
+    }
+    print(business_to_reviews)
+
     return business_to_reviews
 
 def main():
@@ -80,10 +102,15 @@ def main():
             ]
         }
     ]
+
     business_to_reviews = formatting_businesses(raw_business_review_data)
 
+    business_name = prompt_for_business_name()
+
+    
+
     for business_item in business_to_reviews:
-        print('\n\n - Business:  ', self.business_name, '\n     Reviews:  ', self.reviews_fields)
+        print('Finale Print:  ', business_item)
 
     
     
