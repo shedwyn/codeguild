64167cee498560cf82ab5ae0eb78fd5614c44b9c
elfough <shedwyn@gmail.com>
2016-04-05 22:59:40 -0700

    latest version, includes average rating

--- reviews.py
+++ reviews.py
@@ -1,84 +1,68 @@
+from decimal import Decimal
+
+
 class Review:
 
     def __init__(self, rating, review_text):
         self.rating = rating
         self.review_text = review_text
 
-        return None
-
-    def __str__(self):
-        return 'Rating: {}, Text: {}'.format(self.rating, self.review_text)
+    def __repr__(self):
+        return 'Review({}, {})'.format(self.rating, self.review_text)
 
 
 class Business:
-
-    def __init__(self, business_name, reviews_fields):
+    def __init__(self, business_name, reviews):
         self.business_name = business_name
-        self.reviews_fields = reviews_fields
+        self.reviews = reviews
 
-        return None
-
-    def __str__(self):
-        """magic string to format print feature"""
-        return 'Reviews for {}: {})'.format(self.business_name, self.reviews_fields)
+    def __repr__(self):
+        """magic repr to format print feature"""
+        return 'Business({}, {})'.format(self.business_name, self.reviews)
 
 
-    def find_avg_rating(self, reviews_fields):
+    def find_avg_rating(self, business_to_reviews):
         """takes reviews fields for business, calc avg on rating, return avg"""
-
-        rating_sum = sum(review_item[0] for review_item in reviews_fields)
-        rating_avg = rating_sum/len(reviews_fields)
-
-        return rating_avg
-
-        #  CHRIS:  I am thinking we might be able to do this better - unpack and format the list of reviews
-            #  into individual lines.  What do you think?
-
-        return 'Business: {}, Reviews {}'.format(self.business_name, self.reviews_fields)
-
+        basket = Decimal(sum([review.rating for review in self.reviews])/len(self.reviews))
+        return round((basket), 1)
     
-# ********************** END OF CLASSES *****************************
-
+# ********************** END OF CLASSES ****************************
 def prompt_for_business_name():
     """ask user for business name, return business name"""
     business_name = input('Business Name:  ')
     return business_name
 
-
 def format_single_review(review_item):
-    
+    """takes in review, instantiate, return instantiated review"""
     single_review = Review(review_item['rating'], review_item['text'])
-    
-    print('Single Review: ', single_review)
-    
     return single_review
 
 def formatting_reviews(list_reviews):
-     
+    """takes in list a reviews, reformats and instantiates each list item, returns instantiated
+    list"""
     business_reviews = [format_single_review(review_item) for review_item in list_reviews]
-
-    print('Business Reviews:', business_reviews)          
-
     return business_reviews
 
 def format_single_business(business_item):
-
+    """takes in business item, instantiates, returns instatiated business"""
     business_reviews = formatting_reviews(business_item['reviews'])
     single_business = Business(business_item['business_name'], business_reviews)
-    print('Single Business:  ', single_business)
-
     return single_business
 
 def formatting_businesses(raw_business_review_data):
-    
-    business_to_reviews = {
+    """takes in raw data, creates a list of businesses and reviews properly instatiated, returns
+    list"""
+    business_and_reviews = [
         format_single_business(
             business_item
             )
         for business_item in raw_business_review_data
-    }
-    print(business_to_reviews)
+    ]
+    return business_and_reviews
 
+def create_business_to_reviews(business_and_reviews):
+    """takes instatiated list, creates dictionary with business name as key, returns dictionary"""    
+    business_to_reviews = {business.business_name:business for business in business_and_reviews}
     return business_to_reviews
 
 def main():
@@ -103,18 +87,16 @@ def main():
         }
     ]
 
-    business_to_reviews = formatting_businesses(raw_business_review_data)
+    business_and_reviews = formatting_businesses(raw_business_review_data)
 
-    business_name = prompt_for_business_name()
+    business_to_reviews = create_business_to_reviews(business_and_reviews)
 
-    
+    search_name = prompt_for_business_name()
 
-    for business_item in business_to_reviews:
-        print('Finale Print:  ', business_item)
+    average_rating = Business.find_avg_rating(business_to_reviews[search_name], business_to_reviews)
 
-    
-    
-# ********************** END OF DEFINING FUNCTIONS **************************
+    print('The average rating for {} was'.format(search_name), average_rating)
+
 
 main()
 
@@ -126,7 +108,7 @@ main()
 # CODE SCRAP HEAP ***********************************************************
 
 # # one_business_dict = {'business_name': 'Salt & Straw', 'reviews': [{'rating': 5, 'text': 'Lucious ice cream!'}, {'rating': 4, 'text': 'Super tasty, but such a long line!'}]}
-    
+
 
 #     reviews = formatting_review(raw_reviews)
 
@@ -135,9 +117,8 @@ main()
 # # single_business = Business(one_business_dict['business_name'], [single_review])  # one_business_dict['reviews'])
 
 
-    
-    
+
+
 
 # print()
     # business_name = prompt_for_business_name()
-
