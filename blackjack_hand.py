f0311d36ce82af64296ba343a0e2427e357d9d2e
elfough <shedwyn@gmail.com>
2016-04-07 12:17:43 -0700

    all basic functions working, add card, update score, but no exiting program!

--- blackjack_hand.py
+++ blackjack_hand.py
@@ -28,11 +28,10 @@ class Card:
             ('queen', 10), 
             ('king', 10)
         ]
-        card_score = 0
+        #card_score = 0
         for item in scores:
             if card.card_rank == item[0]:
                 card_score = item[1]
-        print(card_score)
         return card_score
 
 
@@ -43,121 +42,95 @@ class Hand:
     def __repr__(self):
         return 'Hand({})'.format(self.cards)
 
-    
-
-
     def give_score(user_hand):
-        """return lose message if score_hand() > 21"""
+        """take in hand, tally current hand, return total score"""
   
         scoring_list = []
         
         for card in user_hand.cards:
             scoring_list.append(Card.score_single_card(card))
 
-        print [scoring_list]
+        if 'ace' in user_hand.cards and sum(scoring_list) < 11:
+            scoring_list.append(10)
 
         hand_score = sum(scoring_list)
-
-        print(hand_score)
 
         return hand_score
 
 
 
-
-    def score_hand(hand_score):
-    """returns total score of final hand"""
-
-        if score_hand(user_hand) < 21:
-            add_card_to_hand(user_hand)
-
-        elif score_hand() == 21:
-                print('winner, winner, chicken dinner!')
-        else:
-                print('wah, wah, wah...busted!')
-
 def prompt_user_for_suit():
     """ask user for suit of card, return suit"""
-    user_suit = input('\nWhat suit to use for card? ')
+    return input('\nWhat suit to use for card? ')
 
-    return suit
-
-
-def prompt_user_for_card():
-    """ask user for hand, instantiate to Card, return instantiated card"""
-    
-    
-    user_rank = input('Which card is it? ')
-    user_card = Card(user_suit, user_rank)
-    
-    return user_card
-
-def format_single_card(suit, rank):
+def prompt_user_for_rank():
     """ask user for rank, return rank"""
+    return input('\nWhich card is it? ')
+
+def format_single_card():
+    """takes nothing, prompts for card variables, returns instantiated card"""
     print(
         'For ease of data entry, use the following abbreviations:',
         '\n SUITS:  clubs = cl, spades = sp, hearts = he, and diamonds = di',
         '\n CARD RANK: ace, jack, queen, king',
         '\n    cards ranked with numbers are 2 - 10'
     )
-
-    suit = prompt_user_for_suit()
-    rank = prompt_user_for_card()
-
+    card_suit = prompt_user_for_suit()
+    card_rank = prompt_user_for_rank()
+    user_card = Card(card_suit, card_rank)
     return user_card
 
-
-def format_user_hand():
-    
-
-
-    
-    return user_hand
+def format_user_hand(user_card1, user_card2):
+    """takes in initial deal cards, instantiates to Hand, returns instatiated hand"""
+    return Hand([user_card1, user_card2])
 
 def draw_new_card():
-    """adds a card to a hand"""
+    """takes nothing, creates new card, returns updated hand"""
+    return format_single_card()
 
-    new_card = prompt_user_for_card()
-    user_hand.cards.append
+def add_card_to_hand(user_hand):
+    """takes new card and user_hand, adds card to user_hand, returns new_hand"""
+    print(user_hand)
+    new_card = draw_new_card()
+    user_hand.cards.append(new_card)
+    print(user_hand)
+    produce_final_score(user_hand)
+
+    return 
 
 
+def produce_final_score(user_hand):
+    """returns total score of final hand"""
+    
+    score = user_hand.give_score()
 
+    while score < 21:
+        add_card_to_hand(user_hand)
+
+    if user_hand.give_score() == 21:
+        print('winner, winner, chicken dinner!')
+    else:
+        print('wah, wah, wah...busted!')
+
+    
 
 def schematic():
     """functions that run the program, returns nothing"""
-
-    user_card1 = prompt_user_for_card()
-    user_card2 = prompt_user_for_card()
-    user_hand = Hand([user_card1, user_card2])
-    print(user_card1, user_card2, user_hand)
-
     # suits = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
-    # card1 = Card('cl', '9')
-    # card2 = Card('he', 'ace')
-    # user_hand = Hand([card1, card2])
+    user_card1 = Card('cl', '9')  #format_single_card()
+    user_card2 = Card('he', 'ace')  #format_single_card()
+    user_hand = format_user_hand(user_card1, user_card2) 
+    print(user_hand)
+    
+    #add_card_to_hand(user_hand)
+    #print('second hand', user_card1, user_card2, user_hand)
+    #user_hand = Hand([card1, card2])
 
-    format_user_hand()
-    #give_score(user_hand)
-    #hand_score = Hand.give_score(user_hand) # store somewhere to be redrawn?
+    print(user_hand.give_score())
 
-
-    draw_new_card()
-
-
-
-
-
-
+    produce_final_score(user_hand)
 
 
     
-
-    #score_hand(hand_score)
-
-    
-
-
-
-
 
 schematic()
