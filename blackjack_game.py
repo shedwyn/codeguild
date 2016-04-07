4b6341fa5c18596befa5ded6459f27b21c4aa4fa
elfough <shedwyn@gmail.com>
2016-04-07 16:34:51 -0700

    able to deal 2 cards to both players

--- blackjack_game.py
+++ blackjack_game.py
@@ -3,7 +3,7 @@ import random
 
 class Card:
     suits = ['clubs', 'spades', 'hearts', 'diamonds']
-    rank = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+    ranks = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
 
     def __init__(self, suit, card_rank):
         """takes suit and card_val where card_val == 'Ace', '2', '3'...'King'"""
@@ -48,12 +48,22 @@ class Hand:
                 hand_score += 10
         return hand_score
 
-def Deck:
+
+class Deck:
     def __init__(self, cards):
+        """takes in self and list of cards"""
         self.cards = cards
 
     def __repr__(self, cards):
         return 'Deck({})'.format(self.cards)
+
+    def deal_hand(self):
+        """takes in nothing.  deals 2 cards to player and to dealer"""
+        random.shuffle(self.cards)
+        card1 = self.cards.pop()
+        card2 = self.cards.pop()
+        hand = Hand([card1, card2])
+        return hand
 
 
 def prompt_user_for_suit():
@@ -65,35 +75,30 @@ def prompt_user_for_rank():
     return input('\nWhich card is it? ')
 
 def create_deck():
-    """takes nothing, creates deck, returns deck"""
+    """takes nothing, instantiates Card for single cards and instantiates
+    Deck.  returns deck dictionary"""
+    suits = ['clubs', 'spades', 'hearts', 'diamonds']
+    ranks = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+    deck_dict = {}
+    deck_list = []
+    for suit in suits:
+        card_suit = suit
+        for rank in ranks:
+            card_rank = rank
+            card_name = '{} | {}'.format(card_suit, card_rank)
+            single_card = format_single_card(suit, rank)  #rmv later
+            deck_dict[card_name] = single_card  #rmv later
+            deck_list.append(single_card)
+    deck_cards = Deck(deck_list)
+    return deck_cards
 
 
 
-    return
 
 
-
-
-
-def deal_initial_hand_cards():
-
-
-    return hand
-
-
-
-def format_single_card():
-    """takes nothing, prompts for card variables, returns instantiated card"""
-    print(
-        'For ease of data entry, use the following abbreviations:',
-        '\n SUITS:  clubs = cl, spades = sp, hearts = he, and diamonds = di',
-        '\n CARD RANK: ace, jack, queen, king',
-        '\n    cards ranked with numbers are 2 - 10'
-    )
-    card_suit = prompt_user_for_suit()
-    card_rank = prompt_user_for_rank()
-    user_card = Card(card_suit, card_rank)
-    return user_card
+def format_single_card(suit, rank):
+    """takes in suit & rank, prompts for card variables, returns instantiated card"""
+    return Card(suit, rank)
 
 def format_user_hand(user_card1, user_card2):
     """takes in initial deal cards, instantiates to Hand, returns instatiated hand"""
@@ -124,13 +129,39 @@ def produce_final_score(user_hand):
 
 def schematic():
     """functions that run the program, returns nothing"""
-    user_card1 = format_single_card()
-    user_card2 = format_single_card()
-    user_hand = format_user_hand(user_card1, user_card2) 
-    print('You were dealt these cards:  ', user_hand)
-    produce_final_score(user_hand)
+    deck_cards = create_deck()
+    print(len(deck_cards.cards))
+
+    player_cards = deck_cards.deal_hand()
+    print(player_cards)
+
+    print(len(deck_cards.cards))
+
+    computer_cards = deck_cards.deal_hand()
+    print(computer_cards)
+
+    print(len(deck_cards.cards))
+
+
+
+
+
+
+
+
+
+    # user_card1 = format_single_card()
+    # user_card2 = format_single_card()
+    # user_hand = format_user_hand(user_card1, user_card2) 
+    
+    # print('You were dealt these cards:  ', user_hand)
+    
+    # produce_final_score(user_hand)
 
 
 schematic()
+
+
+
 # hand = Hand([Card('cl', '2'), Card('cl', 'ace')])
 # produce_final_score(hand)
