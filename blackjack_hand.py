5506593c609ac95a726c1e0f33b71881afafedf8
elfough <shedwyn@gmail.com>
2016-04-07 09:48:59 -0700

    added score function

--- blackjack_hand.py
+++ blackjack_hand.py
@@ -19,56 +19,82 @@ class Hand:
     def __repr__(self):
         return 'Hand({})'.format(self.cards)
 
+    def give_score(user_hand):
+        """return lose message if score_hand() > 21"""
 
-suits = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
+        scores = [
+            ('ace', 1), 
+            ('2', 2), 
+            ('3', 3), 
+            ('4', 4), 
+            ('5', 5), 
+            ('6', 6), 
+            ('7', 7), 
+            ('8', 8), 
+            ('9', 9), 
+            ('10', 10), 
+            ('jack', 10), 
+            ('queen', 10), 
+            ('king', 10)
+        ]
+        scoring_list = []
+        for card in user_hand:
+            card_score = item[1] in scores if card.card_rank == item[1] for item in scores
+            scoring_list.append(card_score)
+        
+        print(scoring_list)
 
-rank = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-
-Jack = Card('Clubs', 'Jack')
+        return hand_score
 
 
+        # if score_hand(user_hand) < 21:
+        #     add_card_to_hand(user_hand)
 
-def add_card_to_hand():
+        # elif score_hand() == 21:
+        #     print('winner, winner, chicken dinner!')
+        # else:
+        #     print('wah, wah, wah...busted!')
+
+def add_card_to_hand(user_hand):
     """adds a card to a hand"""
 
-
-def score_hand():
+def score_hand(user_hand, scores):
     """returns total score of final hand"""
 
 
-def give_score():
-    """return lose message if score_hand() > 21"""
 
+    return None
 
 def prompt_user_for_hand():
     """ask user for hand, run functions to score, return score"""
     print(
         'For ease of data entry, use the following abbreviations:',
-        '\n SUITS:  Clubs = CL, Spades = SP, Hearts = HE, and Diamonds = DI',
-        '\n CARD RANK: Ace = ac, Jack = ja, Queen = qu, King = ki',
+        '\n SUITS:  clubs = cl, spades = sp, hearts = he, and diamonds = di',
+        '\n CARD RANK: ace, jack, queen, king',
         '\n    cards ranked with numbers are 2 - 10'
     )
-    user_suit1 = input('\nWhat suit to use for card 1? ')
-    user_rank1 = input('Which card is it? ')
-    user_suit2 = input('\nWhat suit to use for card 2? ')
-    user_rank2 = input('Which card is it? ')
-    user_cards = [(user_suit1, user_rank1), (user_suit2, user_rank2)]
+    user_suit = input('\nWhat suit to use for card? ')
+    user_rank = input('Which card is it? ')
+    user_card = (user_suit, user_rank)
     
-    return user_cards
+    return user_card
 
 def format_user_hand():
-    user_cards = prompt_user_for_hand()
-    user_card1 = Card(user_cards[0][0], user_cards[0][1])
-    user_card2 = Card(user_cards[1][0], user_cards[1][1])
+    user_card1 = prompt_user_for_hand()
+    user_card2 = prompt_user_for_hand()
     user_hand = Hand([user_card1, user_card2])
     print(user_card1, user_card2, user_hand)
 
     return user_hand
 
-
 def schematic():
-
-    format_user_hand()
+    #suits = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
+    card1 = Card('cl', '9')
+    card2 = Card('he', 'ace')
+    user_hand = Hand([card1, card2])
+    #format_user_hand()
+    #give_score(user_hand)
+    Hand.give_score(user_hand)
 
 
 schematic()
