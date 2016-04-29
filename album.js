332a8d35368ce03f380a5d51774ea098e8790184
elfough <shedwyn@gmail.com>
2016-04-29 13:30:38 -0700

    js project to update photo album

---
+++ album.js
@@ -0,0 +1,48 @@
+"use strict"
+
+function getURLString(){
+    return $("#urlAddress").val();
+};
+
+function createDelLink(sectionElement){
+    var deleteElement = $("<a></a>").text("Delete").attr("href", "");
+    deleteElement.on("click", function (event) {
+        event.preventDefault();
+        sectionElement.remove();
+    });
+    return deleteElement;
+};
+
+function createImageSection(URLString){
+    // var albumChild = document.createElement("section");
+    var imageElement = $("<img>").attr("src", URLString);
+    var linkElement = $("<a></a>").text("click to download").attr("href", URLString);
+    var albumChild = $("<section></section>").append(imageElement);   
+    albumChild.append(linkElement);
+    var deleteElement = createDelLink(albumChild);
+    albumChild.append(deleteElement);
+    return albumChild;
+};
+
+function addAlbumSectionBlock(){
+    var URLString = getURLString();
+    var sectionElement = createImageSection(URLString);
+    $("#albumlist").append(sectionElement); // Adding album section child to parent section
+};
+
+function addPictureControlSet() {
+    $("form").on("submit", function (event) {
+        event.preventDefault();
+        addAlbumSectionBlock();
+        countImages();
+    });
+};
+function countImages(){
+    var parent = $("#albumlist");
+    var childCount = parent.children().length;
+    $("#imagecount").text("Count of Images:  " + childCount);
+};
+$(document).ready(function(){
+    //this could be the equivalent of jquery main
+    addPictureControlSet();
+});
