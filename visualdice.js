438509ce104cae055d92d8f2d8885711d537dbe2
elfough <shedwyn@gmail.com>
2016-05-02 10:00:06 -0700

    beggining outline of Visual Dice assignment

---
+++ visualdice.js
@@ -0,0 +1,8 @@
+"use strict"
+// Give the user a number input box with a button "roll". When they click that 
+    // button, make that many 6-sided dice appear on the screen.
+// The dice should appear visually as dice, although for testing you can just 
+    // start with numbers. Come up with any reasonable way to display the visual dice.
+// If the user clicks any of the dice, it re-rolls just that one. If they re-click 
+    // the roll button, erase the dice and roll new ones.
+// At the bottom of the screen, show the sum of all the dice currently out.
