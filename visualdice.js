cfb8a7e947984bdb6679ac2f115df149f17d8b3b
elfough <shedwyn@gmail.com>
2016-05-02 10:45:17 -0700

    updated with images for dice, flexbox for dice, beginning js code

--- visualdice.js
+++ visualdice.js
@@ -1,4 +1,39 @@
 "use strict"
+
+function getNumberToRoll(){
+    return $("#numberToRoll").val()
+}
+function rollSingleDie(){
+
+}
+function addDieToDiceArea(){
+    var numberOfRolls = getNumberToRoll();
+    for (var i = 0; i < numberOfRolls; i += 1) {
+        var dieValue = rollSingleDie();
+        $("#diceArea").append(dieValue);
+    };
+    //Adding children to parent section
+}
+function deleteSingleDie(){
+
+}
+function eraseEntireHand(){
+
+}
+function tallyHand(){
+    var parent = $("#diceArea");
+    var childCount = parent.children().length;
+    $("#tallyHand").text("Simple Dice Score:  " + childCount);
+}
+var diceImages = {
+    1:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_1.png">,
+    2:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_2.png">,
+    3:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_3.png">,
+    4:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_4.png">,
+    5:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_5.png">,
+    6:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_6.png">,
+}
+
 // Give the user a number input box with a button "roll". When they click that 
     // button, make that many 6-sided dice appear on the screen.
 // The dice should appear visually as dice, although for testing you can just 
