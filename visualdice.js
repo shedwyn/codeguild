cd4182b11bf05abe7dd795b56f5150d5229baeed
elfough <shedwyn@gmail.com>
2016-05-02 11:48:25 -0700

    js allows creation of <img> now using random roll, still cannot get Roll button to work

--- visualdice.js
+++ visualdice.js
@@ -1,39 +1,47 @@
-"use strict"
+"use strict";
 
 function getNumberToRoll(){
     return $("#numberToRoll").val()
 }
 function rollSingleDie(){
-
+    return _.random(1, 6)
+}
+function createDieImage(){
+    var dieImageLocation = diceImages[rollSingleDie()];
+    return $("<img>").attr("src", dieImageLocation)
 }
 function addDieToDiceArea(){
     var numberOfRolls = getNumberToRoll();
     for (var i = 0; i < numberOfRolls; i += 1) {
-        var dieValue = rollSingleDie();
-        $("#diceArea").append(dieValue);
+        $("#diceArea").append(createDieImage());
     };
     //Adding children to parent section
-}
-function deleteSingleDie(){
-
-}
-function eraseEntireHand(){
-
 }
 function tallyHand(){
     var parent = $("#diceArea");
     var childCount = parent.children().length;
     $("#tallyHand").text("Simple Dice Score:  " + childCount);
 }
-var diceImages = {
-    1:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_1.png">,
-    2:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_2.png">,
-    3:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_3.png">,
-    4:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_4.png">,
-    5:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_5.png">,
-    6:<img src="http://www.wpclipart.com/recreation/games/dice/.cache/die_face_6.png">,
+function runIntialRoll(){
+    $("form").on("submit", function (event){
+        event.preventDefault();
+        addDieToDiceArea();
+        tallyHand();
+    })
 }
-
+function deleteSingleDie(){
+}
+function eraseEntireHand(){
+}
+var diceImages = {
+    "1":"http://www.wpclipart.com/recreation/games/dice/.cache/die_face_1.png",
+    "2":"http://www.wpclipart.com/recreation/games/dice/.cache/die_face_2.png",
+    "3":"http://www.wpclipart.com/recreation/games/dice/.cache/die_face_3.png",
+    "4":"http://www.wpclipart.com/recreation/games/dice/.cache/die_face_4.png",
+    "5":"http://www.wpclipart.com/recreation/games/dice/.cache/die_face_5.png",
+    "6":"http://www.wpclipart.com/recreation/games/dice/.cache/die_face_6.png",
+};
+console.log(createImageElement());
 // Give the user a number input box with a button "roll". When they click that 
     // button, make that many 6-sided dice appear on the screen.
 // The dice should appear visually as dice, although for testing you can just 
