5f410a8454804397939c2d054ea050b3e6398bd2
elfough <shedwyn@gmail.com>
2016-05-04 16:19:59 -0700

    updated js with proper breakdown of functions but only for name

--- validator.js
+++ validator.js
@@ -1,33 +1,16 @@
 "use strict";
 
 function checkFieldEmpty(anyObject){
-    if (anyObject === "") {
-        return true
+    if (anyObject !== "") {
+        return false
     }
     else {
-        return false
+        return true
     }
     // can I use jQuery.isEmptyObject($("#fullName"))?
 }
 function checkNameCount(namesList){
-    if (namesList.length < 2) {
-        return false
-    }
-    if (namesList.length > 2) {
-        return false
-    }
-    else {
-        return true
-    }
-}
-function checkNameLength(namesList) {
-    if (checkNameCount(namesList) === false) {
-        return false
-    }
-    if (namesList[0].length <= 1) {
-        return false
-    }
-    else if (namesList[1].length <= 1) {
+    if (namesList.length !== 2) {
         return false
     }
     else {
@@ -39,144 +22,144 @@ function checkNameCapital(namesList){
     // people who type only first initial
     var firstName = namesList[0];
     var lastName = namesList[1];
-    if (checkNameCount(namesList) === false) {
-        return false
-    }
-    else if (!(firstName[0] === firstName[0].toUpperCase())) {
-        return false
-    }
-    else if (!(lastName[0] === lastName[0].toUpperCase())) {
+    if (checkNameCount(namesList) !== true || 
+        firstName[0] !== firstName[0].toUpperCase() ||
+        lastName[0] !== lastName[0].toUpperCase()
+        ){
         return false
     }
     else {
         return true
     }
 }
-function checkFullName(){
-    var fullName = $("#fullName").val();
+function checkFullName(fullName){
+    
     var namesList = fullName.split(" ");
     var fieldName = "Full Name"
-    if (checkFieldEmpty(fullName) === true){
-        $("#warnings").empty();
-        return true
-    }
-    else if (checkNameCount(namesList) === false) {
-        $("#fullName").css("background-color", "yellow");
-        $("#warnings").append(formatWarningNotice(fieldName));
-        return false
-    }
-    else if (checkNameLength(namesList) === false) {
-        $("#fullName").css("background-color", "yellow");
-        $("#warnings").append(formatWarningNotice(fieldName));
-        return false
-    }
-    else if (checkNameCapital(namesList) === false) {
-        $("#fullName").css("background-color", "yellow");
-        $("#warnings").append(formatWarningNotice(fieldName));
-        return false
-    }
-    else {
-        $("#warnings").empty();
-        return true
-    }
-}
-function checkBirthDate(){
-    var regexLayout = /^(\d{4})(\-)(\d{2})(\-)(\d{2})$/
-    var birthDate = $("#birthDate").val()
-    var fieldName = "Birth Date"
-    console.log(birthDate.match(regexLayout))
-    if (!(birthDate.match(regexLayout))) {
-        $("#birthDate").css("background-color", "yellow");
-        $("#warnings").append(formatWarningNotice(fieldName));
+    if (checkFieldEmpty(fullName) === false && checkNameCount(namesList) === false || 
+        checkFieldEmpty(fullName) === false && checkNameCapital(namesList) === false) {
         return false
     }
     else {
         return true
     }
 }
-function checkPhoneNum(){
-    // 555-555-5555
-    //^(\d{3})(\-)(\d{3})(\-)(\d{4})$
-}
-function formatWarningNotice(fieldName){
-    var warningNotice = $("<p></p>").text("WARNING!  You are in danger! The " + fieldName +
-        " field you have been editing is incorrect.  Take a look at the example given " +
-        "and try again, mmm-Kay?  What are you waiting for?  GO NOW!")
-        .css("background-color", "yellow")
-    return warningNotice
-}
-function onFieldBlur(id, fieldCheck) {
-    $("#id").on("blur", function(event){
-        event.preventDefault();
+function updateWarningArea(boolVal){
+    if (boolVal === false) {
+        $("#fullName").css("background-color", "yellow");
+        // $("#warnings").append(formatWarningNotice(fieldName));
+    }
+    else {
         $("#warnings").empty();
-        fieldCheck;
-    })
+        $("#fullName").css("background-color", "initial")
+    }
 }
-function formatApprovalNotice(){
-    var approvalNotice = $("<p></p>").text("CONGRATULATIONS!  Your data has been " +
-        "accepted.  Unfortunately, you are now above the age of mandatory death.  " +
-        "A team of mutant giraffes has been deployed to your present location to " +
-        "terminate you.  Have a nice day.")
-        .css("background-color", "green")
-    return approvalNotice
-    //approvalNotice.css("background-color", "green")
+function runNameCheckMain(){
+    var fullName = $("#fullName").val();
+    var booleanName = checkFullName(fullName);
+    updateWarningArea(booleanName)
 }
-function formatMalformNotice(){
-    var malformNotice = $("<p></p>").text("WARNING!  Malformed information has " +
-        "been presented to your evil overlords!  This kind of egregious mistake " +
-        "cannot be ignored.  A team of ninjas ('assassin' being redundant) is now " +
-       "being deployed to your location and death will come on swift sword, or " +
-        "nunchucks, we do not like to micro-manage.  END OF WARNING STATEMENT." +
-        " Oh, and please take a few moments to fill out the exit survey before dying.")
-        .css("background-color", "red")
-    return malformNotice
-    //malformNotice.css("background-color", "red")
+function registerInitialCallback(){
+    $("#fullName").on("change", runNameCheckMain)
 }
-function checkFieldsNotEmpty(){
-    var fullName = $("#fullName").val()
-    var birthDate = $("#birthDate").val()
-    var phoneNum = $("#phoneNum").val()
+registerInitialCallback()
+// function checkBirthDate(){
+//     var regexLayout = /^(\d{4})(\-)(\d{2})(\-)(\d{2})$/
+//     var birthDate = $("#birthDate").val()
+//     var fieldName = "Birth Date"
+//     console.log(birthDate.match(regexLayout))
+//     if (!(birthDate.match(regexLayout))) {
+//         $("#birthDate").css("background-color", "yellow");
+//         $("#warnings").append(formatWarningNotice(fieldName));
+//         return false
+//     }
+//     else {
+//         return true
+//     }
+// }
+// function checkPhoneNum(){
+//     // 555-555-5555
+//     //^(\d{3})(\-)(\d{3})(\-)(\d{4})$
+// }
+// function formatWarningNotice(fieldName){
+//     var warningNotice = $("<p></p>").text("WARNING!  You are in danger! The " + fieldName +
+//         " field you have been editing is incorrect.  Take a look at the example given " +
+//         "and try again, mmm-Kay?  What are you waiting for?  GO NOW!")
+//         .css("background-color", "yellow")
+//     return warningNotice
+// }
+// function onFieldBlur(id, fieldCheck) {
+//     $("#id").on("blur", function(event){
+//         event.preventDefault();
+//         $("#warnings").empty();
+//         fieldCheck;
+//     })
+// }
+// function formatApprovalNotice(){
+//     var approvalNotice = $("<p></p>").text("CONGRATULATIONS!  Your data has been " +
+//         "accepted.  Unfortunately, you are now above the age of mandatory death.  " +
+//         "A team of mutant giraffes has been deployed to your present location to " +
+//         "terminate you.  Have a nice day.")
+//         .css("background-color", "green")
+//     return approvalNotice
+//     //approvalNotice.css("background-color", "green")
+// }
+// function formatMalformNotice(){
+//     var malformNotice = $("<p></p>").text("WARNING!  Malformed information has " +
+//         "been presented to your evil overlords!  This kind of egregious mistake " +
+//         "cannot be ignored.  A team of ninjas ('assassin' being redundant) is now " +
+//        "being deployed to your location and death will come on swift sword, or " +
+//         "nunchucks, we do not like to micro-manage.  END OF WARNING STATEMENT." +
+//         " Oh, and please take a few moments to fill out the exit survey before dying.")
+//         .css("background-color", "red")
+//     return malformNotice
+//     //malformNotice.css("background-color", "red")
+// }
+// function checkFieldsNotEmpty(){
+//     var fullName = $("#fullName").val()
+//     var birthDate = $("#birthDate").val()
+//     var phoneNum = $("#phoneNum").val()
 
-    if (fullName === "") {
-        return false
-    }
-    else if (birthDate === "") {
-        return false
-    }
-    else if (phoneNum === "") {
-        return false
-    }
-    else {
-        return true
-    }
-}
-function validateForm(){
-    // posts a success or error notice
-    $("#warnings").empty();
-    if (checkFieldsNotEmpty() === false) {
-        $("#warnings").append(formatMalformNotice())
-    }
-    else if (checkFullName() === false) {
-        $("#warnings").append(formatMalformNotice())
-    }
-    // else if (checkBirthDate() === false) {
-    //     $("#warnings").append(formatMalformNotice())
-    // }
-    // else if (checkPhoneNum() === false) {
-    //     $("#warnings").append(formatMalformNotice())
-    // }
-    else {
-        $("#warnings").append(formatApprovalNotice())
-    }
-}
-function mainFunction(){
-    $("#fullName").on("change", onFieldBlur(fullName, checkFullName()));
-    $("#birthDate").on("change", onFieldBlur(birthDate, checkBirthDate()));
-    $("phoneNum").on("change", onFieldBlur(phoneNum, checkPhoneNum()));
-    $("form").on("submit", function (event){
-        event.preventDefault();
-        validateForm();
-    });
-}
-mainFunction();
+//     if (fullName === "") {
+//         return false
+//     }
+//     else if (birthDate === "") {
+//         return false
+//     }
+//     else if (phoneNum === "") {
+//         return false
+//     }
+//     else {
+//         return true
+//     }
+// }
+// function validateForm(){
+//     // posts a success or error notice
+//     $("#warnings").empty();
+//     if (checkFieldsNotEmpty() === false) {
+//         $("#warnings").append(formatMalformNotice())
+//     }
+//     else if (checkFullName() === false) {
+//         $("#warnings").append(formatMalformNotice())
+//     }
+//     // else if (checkBirthDate() === false) {
+//     //     $("#warnings").append(formatMalformNotice())
+//     // }
+//     // else if (checkPhoneNum() === false) {
+//     //     $("#warnings").append(formatMalformNotice())
+//     // }
+//     else {
+//         $("#warnings").append(formatApprovalNotice())
+//     }
+// }
+// function mainFunction(){
+//     $("#fullName").on("change", onFieldBlur(fullName, checkFullName()));
+//     // $("#birthDate").on("change", onFieldBlur(birthDate, checkBirthDate()));
+//     // $("phoneNum").on("change", onFieldBlur(phoneNum, checkPhoneNum()));
+//     $("form").on("submit", function (event){
+//         event.preventDefault();
+//         validateForm();
+//     });
+// }
+// mainFunction();
 
