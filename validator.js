b494efcfd0f13185250f8e7c6e40acd8712c259c
elfough <shedwyn@gmail.com>
2016-05-03 21:09:14 -0700

    date not working

--- validator.js
+++ validator.js
@@ -1,7 +1,7 @@
 "use strict";
-//If field is valid or empty, remain blue, otherwise chg to yellow
-function checkNameEmpty(fullName){
-    if (fullName === "") {
+
+function checkFieldEmpty(anyObject){
+    if (anyObject === "") {
         return true
     }
     else {
@@ -9,73 +9,98 @@ function checkNameEmpty(fullName){
     }
     // can I use jQuery.isEmptyObject($("#fullName"))?
 }
-function checkNameLength(fullName){
-    var namesList = fullName.split(" ");
-    if (namesList.length === 2) {
-        return true
-    }
-    else {
+function checkNameCount(namesList){
+    if (namesList.length < 2) {
         return false
     }
+    if (namesList.length > 2) {
+        return false
+    }
+    else {
+        return true
+    }
 }
-function checkNameCapital(fullName){
-    var namesList = fullName.split(" ");
+function checkNameLength(namesList) {
+    if (checkNameCount(namesList) === false) {
+        return false
+    }
+    if (namesList[0].length <= 1) {
+        return false
+    }
+    else if (namesList[1].length <= 1) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+function checkNameCapital(namesList){
     // but what about triple last names? de Hoya, de Graf 
     // people who type only first initial
-    return true
+    var firstName = namesList[0];
+    var lastName = namesList[1];
+    if (checkNameCount(namesList) === false) {
+        return false
+    }
+    else if (!(firstName[0] === firstName[0].toUpperCase())) {
+        return false
+    }
+    else if (!(lastName[0] === lastName[0].toUpperCase())) {
+        return false
+    }
+    else {
+        return true
+    }
 }
 function checkFullName(){
     var fullName = $("#fullName").val();
-    var nameConditions = [
-        checkNameEmpty(fullName), 
-        checkNameLength(fullName),
-        checkNameCapital(fullName)
-    ]
-    console.log(nameConditions)
-    if (nameConditions[0] === true){
+    var namesList = fullName.split(" ");
+    var fieldName = "Full Name (First Last)"
+    if (checkFieldEmpty(fullName) === true){
+        $("#warnings").empty();
         return true
     }
-    else if (nameConditions[1] === false) {
+    else if (checkNameCount(namesList) === false) {
+        $("#fullName").css("background-color", "yellow");
+        $("#warnings").append(formatWarningNotice(fieldName));
         return false
     }
-    else if (nameConditions[2] === false) {
+    else if (checkNameLength(namesList) === false) {
+        $("#fullName").css("background-color", "yellow");
+        $("#warnings").append(formatWarningNotice(fieldName));
+        return false
+    }
+    else if (checkNameCapital(namesList) === false) {
+        $("#fullName").css("background-color", "yellow");
+        $("#warnings").append(formatWarningNotice(fieldName));
         return false
     }
     else {
-        console.log("Final else statement which is true")
+        $("#warnings").empty();
         return true
     }
-    console.log("if statements were skipped")
-}
-function formatWarningNotice(){
-    var warningNotice = $("<p></p>").text("WARNING!  You are in danger! The field " +
-        "you have been editing is incorrect.  Take a look at the fields highlighted " +
-        "in YELLOW and try to correct them.  We STRONGLY recommend you do so..." +
-        "immediately.")
-        .css("background-color", "yellow")
-    return warningNotice
-
-}
-function checkNameOnChange() {
-    $("#fullName").on("change", function(event){
-        event.preventDefault();
-        if (checkFullName() === false) {
-            $("#fullName").css("background-color", "yellow");
-            $("#warnings").append(formatWarningNotice());
-        }
-        else {
-            $("#fullName").css("background-color", "lightblue");
-            $("#warnings").empty()
-        }
-    })
 }
 function checkBirthDate(){
-    // YYYY-MM-DD
-    //return true or false
+    var regexLayout = "^(\d{4})(\-)(\d{2})(\-)(\d{2})$"
 }
+
 function checkPhoneNum(){
     // 555-555-5555
     //return true or false
+}
+function formatWarningNotice(fieldName){
+    var warningNotice = $("<p></p>").text("WARNING!  You are in danger! The " + fieldName +
+        " field you have been editing is incorrect.  Take a look at the example given " +
+        "and try again, mmm-Kay?  What are you waiting for?  GO NOW!")
+        .css("background-color", "yellow")
+    return warningNotice
+}
+function onNameFieldChange() {
+    $("#fullName").on("change", function(event){
+        event.preventDefault();
+        $("#warnings").empty();
+        checkFullName();
+    })
 }
 function formatApprovalNotice(){
     var approvalNotice = $("<p></p>").text("CONGRATULATIONS!  Your data has been " +
@@ -90,47 +115,60 @@ function formatMalformNotice(){
     var malformNotice = $("<p></p>").text("WARNING!  Malformed information has " +
         "been presented to your evil overlords!  This kind of egregious mistake " +
         "cannot be ignored.  A team of ninjas ('assassin' being redundant) is now " +
-       "being deployed to your location and death will come on swift sword, or" +
-        "nunchucks, we do not like to micro-manage.  END OF WARNING STATEMENT.")
+       "being deployed to your location and death will come on swift sword, or " +
+        "nunchucks, we do not like to micro-manage.  END OF WARNING STATEMENT." +
+        " Oh, and please take a few moments to fill out the exit survey before dying.")
         .css("background-color", "red")
     return malformNotice
     //malformNotice.css("background-color", "red")
 }
-function formatSubmitButton(){
-    
+function checkFieldsNotEmpty(){
+    var fullName = $("#fullName").val()
+    var birthDate = $("#birthDate").val()
+    var phoneNum = $("#phoneNum").val()
+
+    if (fullName === "") {
+        return false
+    }
+    else if (birthDate === "") {
+        return false
+    }
+    else if (phoneNum === "") {
+        return false
+    }
+    else {
+        return true
+    }
 }
+    
 function validateForm(){
     // posts a success or error notice
-    if (checkFullName() === false) {
+    $("#warnings").empty();
+    if (checkFieldsNotEmpty() === false) {
         $("#warnings").append(formatMalformNotice())
     }
-    else if (checkBirthDate() === false) {
+    else if (checkFullName() === false) {
         $("#warnings").append(formatMalformNotice())
     }
-    else if (checkPhoneNum() === false) {
-        $("#warnings").append(formatMalformNotice())
-    }
+    // else if (checkBirthDate() === false) {
+    //     $("#warnings").append(formatMalformNotice())
+    // }
+    // else if (checkPhoneNum() === false) {
+    //     $("#warnings").append(formatMalformNotice())
+    // }
     else {
         $("#warnings").append(formatApprovalNotice())
     }
 }
-checkNameOnChange();
-console.log(checkFullName());
-$("#birthDate");
-$("#phoneNum");
-$("form").on("submit", function (event){
-    event.preventDefault();
-    validateForm();
-})
-// As the user is typing, you should validate that the fields are filled 
-    // in the correct format. 
-        // If the content is not currently valid, the field 
-//              box should be yellow and a yellow warning notice describing which field 
-//              is malformed displayed atop the form. 
-        // If multiple forms are invalid, just 
-//              show one. I don't care which. 
-        // Otherwise, if valid or empty, the fields 
-//              should be default and no warning notice displayed.
+function mainFunction(){
+    checkFullName();
+    onNameFieldChange();
+    checkBirthDate();
 
-// If the user submits with valid info, display a green success notice atop 
-//     the form, otherwise a red error notice.
+    $("form").on("submit", function (event){
+        event.preventDefault();
+        validateForm();
+    });
+}
+mainFunction();
+
