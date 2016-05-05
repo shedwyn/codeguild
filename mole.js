ed11196bb0dbd7d2814c58627077d1c19fc02fdb
elfough <shedwyn@gmail.com>
2016-05-05 11:56:15 -0700

    just the instructions, no more

---
+++ mole.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Save your solution as mole.html, mole.css, and mole.js.
+
+// Make a 5x4 grid of hole images. Every second, randomly pick a hole in the grid and turn it's image into a mole. If the user clicks on a mole image, turn it back into a hole.
+
+// Use the setInterval function to run a callback function periodically.
