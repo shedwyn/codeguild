799557617cd46a36e8367f3d9ee1f6205dbc2bfb
elfough <shedwyn@gmail.com>
2016-05-06 15:53:37 -0700

    added functions to just change src code

--- mole.js
+++ mole.js
@@ -1,35 +1,81 @@
 "use strict";
 
-var holeImage = $("<img>").attr("src", "hole.png")
-var trumpImage =  $("<img>").attr("src", "trump_head.jpg")
-function pickRandomNum(){
-    return _.random(1, 20)
-}
+//inputs
+
+var holeImage = $("<img>").attr("src", "hole.png").attr("id", "holeImage";)
+var trumpImage =  $("<img>").attr("src", "trump_head.jpg").attr("id", "trumpImage";)
+
+//transform
+
+function pickRandomNum(){return _.random(1, 20)}
+
 function assembleHoleID(){
     var randomNum = pickRandomNum();
     return "#hole" + randomNum
 }
 
+//create
 
-
-
-
-function grabHoleImage(){
-    var holeID = assembleHoleID()
+function toggleOffHoleImage(holeID){
+    return $(holeID).children(".holeImage").css("display", "none")
 }
-
-function clickRevertImage(){
-
+function toggleOffTrumpImage(holeID){
+    return $(holeID).children(".trumpImage").css("display", "none")
+}
+function toggleOnHoleImage(holeID){
+    return $(holeID).children(".holeImage").css("display", "block")
+}
+function toggleOnTrumpImage(holeID) {
+    return $(holeID).children(".trumpImage").css("display", "block")
+}
+function changeImageToTrump(holeID) {
+    return $(holeID).children("<img>").attr("src", "trump_head.jpg")
+}
+function changeImageToHole(holeID){
+    return $(holeID).children(".holeImage").css("src", "hole.png")
+}
+function toggleTrumpToHole(holeID){
+    console.log("yes, we got here");
+    toggleOffTrumpImage(holeID);
+    toggleOnHoleImage(holeID)
 }
 
 function launchTrumpIntervals() {
     setInterval(function(){
-    launchFunction; 
+        var holeID = assembleHoleID();
+        console.log(holeID);
+        toggleOffHoleImage(holeID);
+        toggleOnTrumpImage(holeID);
     }, 1000);
+}
+function stopTrumpIntervals() {
+    clearInterval(launchTrumpIntervals);
+}
+
+function clickImageOff(){
+    getHoleID()
+    toggleOffTrumpImage()
+    toggleOnHoleImage()
 }
 
 
 
+//mains
+
+
+
+// register
+
+// $("#playArea").on("click", function (event){
+//     event.preventDefault();
+//     launchTrumpIntervals;
+// });
+$(".moleHole").on("click", function (event){
+    event.preventDefault();
+    var holeID = this.getAttribute("id")
+    console.log(holeID)
+    toggleTrumpToHole(holeID)
+})
 
 
 
@@ -37,31 +83,10 @@ function launchTrumpIntervals() {
 
 
 
+// Make a 5x4 grid of hole images. Every second, randomly pick a hole
 
-
-
-
-
-
-
-// Save your solution as mole.html, mole.css, and mole.js.
-
-// Make a 5x4 grid of hole images. Every second, randomly pick a hole 
-
-// in the grid and turn it's image into a mole. If the user clicks on a 
+// in the grid and turn it's image into a mole. If the user clicks on a
 
 // mole image, turn it back into a hole.
 
 // Use the setInterval function to run a callback function periodically.
-
-// function holeIDs () {
-//     var parent = $("#playArea")
-//     var childCount = parent.children().length
-//     for (i = 0; i <= childCount; i += 1) 
-        
-// }
-
-// var holeIDs = ["hole01", "hole02", "hole03", "hole04", "hole05", 
-//     "hole06", "hole07", "hole08", "hole09", "hole10", "hole11", "hole12",
-//     "hole13", "hole14", "hole15", "hole16", "hole17", "hole18", "hole19",
-//     "hole20"];
