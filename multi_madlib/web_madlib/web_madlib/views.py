be1ed0e7fd1b84d7168ebcbe666e175eec094808
elfough <shedwyn@gmail.com>
2016-05-11 14:16:23 -0700

    catching up

--- multi_madlib/web_madlib/web_madlib/views.py
+++ multi_madlib/web_madlib/web_madlib/views.py
@@ -1,52 +1,60 @@
+"""
+    Python Coding Bootcamp (pdxcodeguild)
+    Code File for web_madlib/views.py
+    by: Erin Fough and Matthew James K on 5/9/2016
+"""
 from django.http import HttpResponse
 
-def render_madlib_1(request):
-    noun1 = request.GET['first_response']
-    noun2 = request.GET['second_response']
-    noun3 = request.GET['third_response']
-    sentence = "{} hugged {} but not {}".format(noun1, noun2, noun3)
+
+def get_httpresponse_to_request(request=[], sentence=''):
+    """
+    This function accepts the sentence for each numbered request, and the request object, and gives back the HttpResponse
+    :param 1: request is the [] Array of the current incommng http request post parameters from the client
+    :param 2: sentence is the main body of the madlib string as a String
+    :returns: the HttpResponse for each of the ^1 url incomming requests.
+    """
     response_positive = "200 OK"
     response_negative = "400 BAD REQUEST "
-    request_array = request.GET.
-    if noun1 == "":
-        return HttpResponse.status
-    elif noun2 == "":
-        return HttpResponse(response_negative + 'second_response was blank')
-    elif noun3 == "":
-        return HttpResponse(response_negative + 'third_response was blank')
+    if request[0] is None or request[0] is '':
+        response = HttpResponse(status = 400)
+        response.write(response_negative + 'noun1 was not provided or blank')
+        return response
+    elif request[1] is None or request[1] is '':
+        response = HttpResponse(status = 400)
+        response.write(response_negative + 'noun2 was not provided or blank')
+        return response
+    elif request[2] is None or request[2] is '':
+        response = HttpResponse(status = 400)
+        response.write(response_negative + 'noun3 was not provided or blank')
+        return response
     else:
-        return HttpResponse(response_positive + ' ' + sentence)
+        response = HttpResponse(status = 200)
+        response.write(response_positive + ' ' + sentence)
+        return response
+
+
+def render_madlib_1(request):
+    post_parameters = []
+    post_parameters.append(request.GET['noun1'] if 'noun1' in request.GET else None) #The key value must be accessible
+    post_parameters.append(request.GET['noun2'] if 'noun2' in request.GET else None) #If the in expression does not work, use request.GET.get() then check for None
+    post_parameters.append(request.GET['noun3'] if 'noun3' in request.GET else None)
+    sentence = "{} hugged {} but not {}".format(post_parameters[0], post_parameters[1], post_parameters[2])
+    return get_httpresponse_to_request(post_parameters, sentence)
 
 
 def render_madlib_2(request):
-    noun1 = request.GET['first_response']
-    noun2 = request.GET['second_response']
-    noun3 = request.GET['third_response']
-    sentence = "{} hugged {} but not {}".format(noun1, noun2, noun3)
-    response_positive = "200 OK"
-    response_negative = "400 BAD REQUEST "
-    if noun1 == "":
-        return HttpResponse.status
-    elif noun2 == "":
-        return HttpResponse(response_negative + 'second_response was blank')
-    elif noun3 == "":
-        return HttpResponse(response_negative + 'third_response was blank')
-    else:
-        return HttpResponse(response_positive + ' ' + sentence)
+    post_parameters = []
+    post_parameters.append(request.GET['noun1'] if 'noun1' in request.GET else None)
+    post_parameters.append(request.GET['noun2'] if 'noun2' in request.GET else None)
+    post_parameters.append(request.GET['noun3'] if 'noun3' in request.GET else None)
+    sentence = "{} troubled {} but not {}".format(post_parameters[0], post_parameters[1], post_parameters[2])
+    return get_httpresponse_to_request(post_parameters, sentence)
 
 
 def render_madlib_3(request):
-    noun1 = request.GET['first_response']
-    noun2 = request.GET['second_response']
-    noun3 = request.GET['third_response']
-    sentence = "{} hugged {} but not {}".format(noun1, noun2, noun3)
-    response_positive = "200 OK"
-    response_negative = "400 BAD REQUEST "
-    if noun1 == "":
-        return HttpResponse(response_negative + 'first_response was blank')
-    elif noun2 == "":
-        return HttpResponse(response_negative + 'second_response was blank')
-    elif noun3 == "":
-        return HttpResponse(response_negative + 'third_response was blank')
-    else:
-        return HttpResponse(response_positive + ' ' + sentence)
+    post_parameters = []
+    post_parameters.append(request.GET['noun1'] if 'noun1' in request.GET else None)
+    post_parameters.append(request.GET['noun2'] if 'noun2' in request.GET else None)
+    post_parameters.append(request.GET['noun3'] if 'noun3' in request.GET else None)
+    sentence = "{} ran for {} but not {}".format(post_parameters[0], post_parameters[1], post_parameters[2])
+    return get_httpresponse_to_request(post_parameters, sentence)
