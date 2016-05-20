2f7dc731c85451c0c2694d4bef99597f39e7761f
elfough <shedwyn@gmail.com>
2016-05-20 11:05:32 -0700

    update

--- todo/todo/logic.py
+++ todo/todo/logic.py
@@ -1,14 +1,16 @@
 from . import models
 
+
 def create_and_save_new_task(task_name):
     new_task = models.Task(task_name=task_name)
     new_task.save()
 
+
 def create_and_save_new_item(item_name, task):
     new_ie = models.Item(
-        item_name = item_name,
-        task = task,
-        )
+        item_name=item_name,
+        task=task,
+    )
     new_ie.save()
 
 
